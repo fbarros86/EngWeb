a63fba20f2d5fb1254a69817d6f1b3eace5fa835
fbarros86 <xiquita.barros@gmail.com>
2023-03-23 11:06:29 +0000

    TPC6

---
+++ TPC6/AppDataSet/routes/index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+var pessoa = require('../controllers/pessoa')
+
+/* GET home page. */
+router.get('/pessoas', function(req, res, next) {
+  pessoa.list()
+    .then(pessoas=>{
+      res.json(pessoas)
+    })
+    .catch(erro =>{
+      res.json(erro)
+    })
+})
+
+router.get('/pessoas/:id', function(req, res, next) {
+  pessoa.getpessoa(req.params.id)
+  .then(pessoa => {
+    res.json(pessoa);
+  })
+  .catch(erro=>{
+    res.json(erro);
+  })
+});
+
+
+router.post('/pessoas', function(req, res, next) {
+  pessoa.addpessoa(req.body).then(pessoa =>{
+    console.log("json(pessoa)")
+    res.json(pessoa);
+  })
+  .catch(erro=>{
+    res.json(erro);
+
+  })
+});
+
+
+router.put('/pessoas/:id', function(req, res, next) {
+  pessoa.editpessoa(req.params.id, req.body).then(pessoa =>{
+    res.json(pessoa);
+  })
+  .catch(erro=>{
+    res.json(erro);
+  })
+});
+
+router.delete('/pessoas/:id', function(req, res, next) {
+  pessoa.deletepessoa(req.params.id).then(pessoa  =>{
+    res.json(pessoa);
+  })
+  .catch(erro=>{
+    res.json(erro);
+  })
+});
+
+module.exports = router;
